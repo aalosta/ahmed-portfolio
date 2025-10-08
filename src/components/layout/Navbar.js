4abb53f1603ec0d22678ec9cd0c31a6c4eba5619
aalosta <ahmed.alosta1@gmail.com>
2025-10-08 22:10:46 +0200

    remove "menu" v1.2

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -260,7 +260,6 @@ const Navbar = () => {
 <div className="sm:hidden">
   <div className="space-y-1 px-4 py-2 bg-gray-900/50">
     <div className="flex justify-between items-center pb-2 border-b border-gray-700">
-      <span className="text-white font-medium">Menu</span>
       <button 
         type="button"
         onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
